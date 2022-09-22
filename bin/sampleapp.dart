e282f89916bd8b5a9f700c8751b4d43ca38e476c
Mahima1111 <mahimavijayan123@gmail.com>
2022-09-22 22:12:41 +0530

    commit

---
+++ bin/sampleapp.dart
@@ -0,0 +1,19 @@
+void main(){
+  // variable declaration
+  //syntax => datatype variablename='value';
+  String name = 'Mahima';
+  int age = 23;
+  String email = 'mahima123@gmail.com';
+  int phno = 9496575329;
+  String college = 'MZC';
+  double mark = 89.99;
+  String course = 'MCA';
+  print('My name is ${name}');
+  print('Iam ${age} years old');
+  print('Mail ID is ${email}');
+  print('My Number is ${phno}');
+  print('My College name is ${college}');
+  print('My score is ${mark}');
+  print('Iam ${course} student');
+
+}
